6336ec91d0f23c8a98deaaf4dd290bcc7b455c76
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-08-01 17:37:59 -0500

    Bridge Information Complete

---
+++ BCRCalcApp/src2/DeckUnitsMData.py
@@ -0,0 +1,14 @@
+class DeckUnitsDB:
+    def __init__(self):
+        self.unitsList={'LF': 'LF', 'SF': 'SF', 'CF': 'CF', 'LS': 'LS',
+         'EA': 'EA', 'SY': 'SY', 'CY': 'CY', 'LB': 'LB',
+          'AC': 'AC', 'STA': 'STA', 'HR': 'HR', 'TON': 'TON',
+           'MG': 'MG', 'MO': 'MO', 'CYC': 'CYC', 'GAL': 'GAL',
+            'SQ': 'SQ', 'DAY': 'DAY', 'LMI': 'LMI', 'MI': 'MI',
+             '$/D': '$/D', 'TF': 'TF', 'WK': 'WK', 'VF': 'VF', 'BAG': 'BAG'}
+
+
+    def getDeckUnitsList(self):
+        return self.unitsList
+
+
