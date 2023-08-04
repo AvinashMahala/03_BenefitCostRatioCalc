27e45786fc91a6b8728d7edea165745f25153fc8
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-08-03 23:34:57 -0500

    Update homepage.py

--- BCRCalcApp/src2/homepage.py
+++ BCRCalcApp/src2/homepage.py
@@ -14,18 +14,41 @@ class Homepage(ttk.Frame):
         self.bridgeId=""
 
     def create_widgets(self):
-        self.homepage_area = ttk.LabelFrame(self, text="HomePage Area")
-        self.homepage_area.pack(fill="both", expand=True)
+        self.homepage_area = ttk.LabelFrame(self, text="HomePage Area", padding=10)
+        self.homepage_area.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-        self.bridgeId_label_var = tk.StringVar(value="Input Bridge ID")  
-        self.bridgeId_label = ttk.Label(self.homepage_area, textvariable=self.bridgeId_label_var)
-        self.bridgeId_label.pack()
+        label_font = ("Arial", 12, "bold")
+        entry_font = ("Arial", 12)
+        button_font = ("Arial", 12, "bold")
 
-        self.bridge_id_entry = ttk.Entry(self.homepage_area)
-        self.bridge_id_entry.pack()
+        self.bridgeId_label_var = tk.StringVar(value="Input Bridge ID")
+        self.bridgeId_label = ttk.Label(self.homepage_area, textvariable=self.bridgeId_label_var, font=label_font)
+        self.bridgeId_label.grid(row=0, column=0, padx=5, pady=5)
+
+        self.bridge_id_entry = ttk.Entry(self.homepage_area, font=entry_font)
+        self.bridge_id_entry.grid(row=1, column=0, padx=5, pady=5)
 
         self.calculation_button = ttk.Button(self.homepage_area, text="Generate Unique Calculation", command=self.generate_calculation)
-        self.calculation_button.pack()
+        self.calculation_button.grid(row=2, column=0, padx=5, pady=10, sticky="ew")
+
+        # # Define a custom style for the button
+        # self.style = ttk.Style()
+        # self.style.configure("Accent.TButton",
+        #                     foreground="white",
+        #                     background="#32a852",  # A nice green color
+        #                     font=button_font,
+        #                     padding=10,
+        #                     width=20)
+
+        # Set a border for the button to make it more visible
+        # self.style.map("Accent.TButton",
+        #             foreground=[('pressed', 'white'), ('active', 'white')],
+        #             background=[('pressed', '#22893d'), ('active', '#22893d')],
+        #             relief=[('pressed', 'sunken'), ('!pressed', 'raised')])
+
+
+
+
 
     def generate_calculation(self):
         bridge_id = self.bridge_id_entry.get().strip()
