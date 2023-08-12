861ad95cb44cdd240f954aec2c4913d428f3d4d2
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-08-12 13:06:24 -0500

    Update database.py

--- BCRCalcApp/src2/database.py
+++ BCRCalcApp/src2/database.py
@@ -19,10 +19,10 @@ class Database:
         self.conn.commit()
 
     def insert_bridge_deck_calc_hist(self, bridge_id, uuid, final_cost):
-        self.cursor.execute("""INSERT INTO BridgeDeckCalcHist VALUES (?, ?, ?, ?)""", (bridge_id, uuid, final_cost, "deck"))
+        self.cursor.execute("""INSERT INTO BridgeDeckCalcHist VALUES (?, ?, ?)""", (bridge_id, uuid, final_cost))
         self.conn.commit()
 
     def insert_bridge_steel_calc_hist(self, bridge_id, uuid, final_cost):
-        self.cursor.execute("""INSERT INTO BridgeDeckCalcHist VALUES (?, ?, ?, ?)""", (bridge_id, uuid, final_cost, "steel"))
+        self.cursor.execute("""INSERT INTO BridgeSteelCalcHist VALUES (?, ?, ?)""", (bridge_id, uuid, final_cost))
         self.conn.commit()
 
